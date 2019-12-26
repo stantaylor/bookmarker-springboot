26936b0dc2584b95b50077133558a98a10c21370
Stan Taylor <stantaylor@gmail.com>
2019-12-25 20:25:11 -0600

    It works!

--- src/main/java/com/stantaylor/bookmarker/controller/BookmarkController.java
+++ src/main/java/com/stantaylor/bookmarker/controller/BookmarkController.java
@@ -4,6 +4,7 @@ import com.stantaylor.bookmarker.model.Bookmark;
 import org.hibernate.Session;
 
 import java.util.Date;
+import java.util.List;
 
 import static com.stantaylor.bookmarker.hibernate.util.HibernateUtil.getSessionFactory;
 
@@ -16,8 +17,26 @@ public class BookmarkController {
         session.save(b);
         session.getTransaction().commit();
         session.close();
-        System.out.println("Successfully created " + b.toString());
         return b.getId();
+    }
+
+    public static void update(Bookmark b) {
+        Session session = getSessionFactory().openSession();
+        session.beginTransaction();
+        Bookmark bm = (Bookmark) session.load(Bookmark.class, b.getId());
+        bm.setTitle(b.getTitle());
+        bm.setUrl(b.getUrl());
+        session.getTransaction().commit();
+        session.close();
+    }
+
+    public static void delete(Integer id) {
+        Session session = getSessionFactory().openSession();
+        session.beginTransaction();
+        Bookmark b = findByID(id);
+        session.delete(b);
+        session.getTransaction().commit();
+        session.close();
     }
 
     public static Bookmark findByID(Integer id) {
@@ -25,5 +44,14 @@ public class BookmarkController {
         Bookmark b = (Bookmark) session.load(Bookmark.class, id);
         session.close();
         return b;
+    }
+
+    public static List<Bookmark> getAll() {
+        Session session = getSessionFactory().openSession();
+        @SuppressWarnings("unchecked")
+        List<Bookmark> bookmarks = session.createQuery("from Bookmark", com.stantaylor.bookmarker.model.Bookmark.class).getResultList();
+        session.close();
+        return bookmarks;
+
     }
 }
--- src/main/resources/seed.sql
+++ src/main/resources/seed.sql
@@ -8,11 +8,5 @@ CREATE TABLE `bookmark` (
   `url` varchar(255) DEFAULT NULL,
   `insert_time` datetime DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
-
-INSERT INTO bookmark(title, url) VALUES ("Google", "https://www.google.com");
-INSERT INTO bookmark(title, url) VALUES ("Facebook", "https://www.facebook.com");
-INSERT INTO bookmark(title, url) VALUES ("Twitter", "https://www.twitter.com");
-
-SELECT * FROM bookmark;
